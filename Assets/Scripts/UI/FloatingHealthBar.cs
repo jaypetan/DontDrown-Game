1d5d13642f4424c2763c1e5d2e60d5af59ff76a8
notbouncee <vinceong2001@gmail.com>
2024-02-04 20:34:10 +0800

    Shark Health Bar
    Added Shark Health Bar UI
    - fixed rotation so it always faces the camera

---
+++ Assets/Scripts/UI/FloatingHealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingHealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Camera camera;
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+
+    public void UpdateHealthBar(float currentValue, float maxValue)
+    {
+        slider.value = currentValue / maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.parent.rotation = camera.transform.rotation;
+        transform.position = target.position + offset;
+    }
+}
